868a696051b81a70c2ef66bc95b032abdf6da448
zwmimi <zwmimi1212@gmail.com>
2022-12-06 14:43:49 +0900

    first-ci

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Form />
+</template>
+
+<script setup lang="ts">
+import Form from './components/Form.vue';
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+  background-color: #292929;
+}
+</style>
---
+++ src/components/Form.vue
@@ -0,0 +1,120 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <form>
+    <div class="form-control">
+      <label for="user-name">Your Name</label>
+      <input id="user-name" name="user-name" type="text" />
+    </div>
+    <div class="form-control">
+      <label for="age">Your Age</label>
+      <input id="age" name="age" type="number" />
+    </div>
+    <div class="form-control">
+      <label for="from">Where Are you from?</label>
+      <select id="from" name="from">
+        <option value="japan">Japan</option>
+        <option value="china">China</option>
+        <option value="others">Others</option>
+      </select>
+    </div>
+    <div class="form-control">
+      <h2>What are you interested in?</h2>
+      <div>
+        <input id="interest-react" name="interest" type="checkbox" />
+        <label for="interest-react">React.js</label>
+      </div>
+      <div>
+        <input id="interest-vue" name="interest" type="checkbox" />
+        <label for="interest-vue">Vue.js</label>
+      </div>
+      <div>
+        <input id="interest-angular" name="interest" type="checkbox" />
+        <label for="interest-angular">Angular.js</label>
+      </div>
+    </div>
+    <div class="form-control">
+      <h2>How do you learn?</h2>
+      <div>
+        <input id="how-video" name="how" type="radio" />
+        <label for="how-video">Video Courses</label>
+      </div>
+      <div>
+        <input id="how-books" name="how" type="radio" />
+        <label for="how-books">Books</label>
+      </div>
+      <div>
+        <input id="how-other" name="how" type="radio" />
+        <label for="how-other">Other</label>
+      </div>
+    </div>
+    <div>
+      <button>Save Data</button>
+    </div>
+  </form>
+</template>
+
+<style scoped>
+form {
+  margin: 2rem auto;
+  max-width: 40rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 2rem;
+  background-color: #ffffff;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+}
+
+h2 {
+  font-size: 1rem;
+  margin: 0.5rem 0;
+}
+
+input,
+select {
+  display: block;
+  width: 100%;
+  font: inherit;
+  margin-top: 0.5rem;
+}
+
+select {
+  width: auto;
+}
+
+input[type="checkbox"],
+input[type="radio"] {
+  display: inline-block;
+  width: auto;
+  margin-right: 1rem;
+}
+
+input[type="checkbox"] + label,
+input[type="radio"] + label {
+  font-weight: normal;
+}
+
+button {
+  font: inherit;
+  border: 1px solid #0076bb;
+  background-color: #0076bb;
+  color: white;
+  cursor: pointer;
+  padding: 0.75rem 2rem;
+  border-radius: 30px;
+}
+
+button:hover,
+button:active {
+  border-color: #002350;
+  background-color: #002350;
+}
+</style>
