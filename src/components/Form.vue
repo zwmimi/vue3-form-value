d1f052f30627565c8527a5f0836fa69445bf84d3
zwmimi <zwmimi1212@gmail.com>
2022-12-06 15:03:54 +0900

    カスタムディレクティブ

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
+import { ref } from "vue";
+
+const userName = ref<string>("");
+
+// v-focus
+const vFocus = {
+  // マウント時に挙動
+  mounted: (el: HTMLElement) => {
+    el.focus();
+  },
+};
 </script>
 
 <template>
   <form>
     <div class="form-control">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" />
+      <input id="user-name" name="user-name" type="text" v-focus />
     </div>
     <div class="form-control">
       <label for="age">Your Age</label>
